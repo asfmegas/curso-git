06dff795f43e4de09902827b6f4d4f5d9265ece8
Alexsandro Facanha <asfsteel2012@gmail.com>
2014-12-25 20:48:24 -0300

    teste e implementação fibonacci 2

--- test/br/com/asfmegas/git/TesteFibonacci.java
+++ test/br/com/asfmegas/git/TesteFibonacci.java
@@ -9,6 +9,8 @@ public class TesteFibonacci {
     @Test
     public void testFibonacci(){
         Fibonacci fibonacci = new Fibonacci();
-        assertEquals(0, fibonacci.calcularFibonacci(0));
+        int[][] casosTeste = {{0,0},{1,1},{2,1}}; 
+        for(int i = 0; i < casosTeste.length; i++)
+            assertEquals(casosTeste[i][1], fibonacci.calcularFibonacci(casosTeste[i][0]));
     }
 }
